6aacd0ca985a525f0240289c9148f552f13b1cf7
UmyaProgrammer <alanusame001@gmail.com>
2025-11-11 00:13:31 +0000

    Comments added to the classes to make them easier to understand.

--- books.py
+++ books.py
@@ -1,5 +1,4 @@
 from datetime import datetime, timedelta, date
-
 class Book:
     def __init__(self, id, title, author, genre, available, due_date, checkouts):
         self.id = id
@@ -10,37 +9,54 @@ class Book:
         self.due_date = due_date
         self.checkouts = checkouts
 
-
     def get_id(self):
+        """
+        :return: ID of the book
+        """
         return self.id
-    
-    
-    def get_title(self):
-        return self.title
-    
-    
-    def get_author(self):
-        return self.author
-    
-    
-    def get_genre(self):
-        return self.genre
-    
-    
-    def get_availability(self):
-        return self.available
-    
-    
-    def get_due_date(self):
-        return self.due_date
-    
-    
-    def get_check_outs(self):
-        return self.checkouts
-    
 
-    def checkout(self)->str:
-        if self.available == True:
+    def get_title(self):
+        """
+        :return: Title of the book
+        """
+        return self.title
+
+    def get_author(self):
+        """
+        :return: Author of the book
+        """
+        return self.author
+
+    def get_genre(self):
+        """
+        :return: Genre of the book
+        """
+        return self.genre
+
+    def get_availability(self):
+        """
+        :return: Availability of the book
+        """
+        return self.available
+
+    def get_due_date(self):
+        """
+        :return: Due date of the book
+        """
+        return self.due_date
+
+    def get_check_outs(self):
+        """
+        :return: Returns the number of times the book was checked out
+        """
+        return self.checkouts
+
+    def checkout(self) -> str:
+        """
+        If the book is not checked out, this function checks it out and creates a deadline to two weeks on the day the book is checked out.
+        :return: Returns a message based on whether the function was successful at checking out the book
+        """
+        if self.available:
             self.available = False
             self.due_date = str(date.today() + timedelta(days=14))
             self.checkouts += 1
@@ -48,12 +64,15 @@ class Book:
         else:
             return f"The book {self.title} has already been checkedout"
 
-
     def return_book(self):
+        """
+        If the book is not already returned, this function will return the book, set its due date to None, and add 1 to the number of times it was checkedout
+        :return: A message based on whether if the book is returned or not
+        """
         if self.available == False:
             self.available = True
             self.due_date = None
-            return f"The book {self.title} has been returned on:\n\t{date.today()}" 
+            return f"The book {self.title} has been returned on:\n\t{date.today()}"
         else:
             return f"The book {self.title} has already been available."
 
@@ -61,6 +80,6 @@ class Book:
         return f"{self.id} - {self.title}\n\t{self.author}\n\tGenre: {self.genre}\n\tAvailability: {self.available}\n\tDue Date: {self.due_date}\n\tCheckouts: {self.checkouts}"
 
 
-
 if __name__ == "__main__":
-    book = new_book = Book(id="B1", title="The Lightning Thief", author="Rick Riordan", genre="Fantasy", available=True, due_date=None, checkouts=2)
+    book = new_book = Book(id="B1", title="The Lightning Thief", author="Rick Riordan", genre="Fantasy", available=True,
+                           due_date=None, checkouts=2)
--- library.py
+++ library.py
@@ -1,33 +1,40 @@
 from books import Book
 from datetime import date
 
+"""
+A class that acts like the foundation of the system by having a list of all the books using the book class to easily checkout, and return books. 
+It also allows the user to search books up based on author and genre. 
+Overall helps the code become more clear and creates a central hub that allows multiple functions to be used with a library object.
+"""
 class Library:
     def __init__(self, list_of_books: Book):
         self.list_of_books = list_of_books
 
     def list_of_available_books(self):
+        """
+        Prints out all the available books.
+        :return: None
+        """
         for book in self.list_of_books:
             if book.get_availability():
                 print(f"{book}\n")
 
-    
     def get_list_of_books(self):
+        """
+        Prints out all the books.
+        :return: None
+        """
         for book in self.list_of_books:
             print(f"{book}\n")
 
-
     def search_by_author(self, author):
+        """
+        The function will go through the list of books and put every book of the given author parameter in a list and then return it.
+        :param author: The function will look for books from this author
+        :return: A list of all the books written by the author
+        """
         matching_books = []
         for book in self.list_of_books():
-            if book.get_author().lower() == author.lower():
-                matching_books.append(book)
-
-        return matching_books
-    
-
-    def search_by_author(self, author):
-        matching_books = []
-        for book in self.list_of_books:
             if book.get_author().lower() == author.lower():
                 matching_books.append(book)
 
@@ -35,6 +42,11 @@ class Library:
 
 
     def search_by_genre(self, genre):
+        """
+        The function will go through the list of books and put every book of the given genre parameter in a list and then return it.
+        :param genre: The function will look for books from this genre
+        :return: A list of all the books written by the genre
+        """
         matching_books = []
         for book in self.list_of_books:
             if book.get_genre().lower() == genre.lower():
@@ -42,8 +54,10 @@ class Library:
 
         return matching_books
 
-
     def get_unique_genres(self):
+        """
+        Prints out a list of all the genres in the library.
+        """
         unique_genres = []
         for book in self.list_of_books:
             if book.get_genre() not in unique_genres:
@@ -52,8 +66,10 @@ class Library:
         for genre in unique_genres:
             print(f"{genre}\n")
 
-
     def get_unique_authors(self):
+        """
+        Prints out a list of all the authors in the library.
+        """
         unique_authors = []
         for book in self.list_of_books:
             if book.get_author() not in unique_authors:
@@ -62,23 +78,33 @@ class Library:
         for author in unique_authors:
             print(f"{author}\n")
 
-
-    def checkout(self, id:str)->str:
+    def checkout(self, id: str) -> str:
+        """
+        Checkouts the book based on the given ID
+        :param id: ID to identify the book the user wants to check out
+        :return: returns whether the library could successfully checked out the book, and if not the reason.
+        """
         for book in self.list_of_books:
             if book.get_id() == id:
                 return book.checkout()
-            
-        
-    def return_book(self, id:str)->str:
+
+    def return_book(self, id: str) -> str:
+        """
+        Returns the book based on the given ID
+        :param id: ID to identify the book the user wants to return
+        :return: returns whether the library could successfully return the book, and if not the reason.
+        """
         for book in self.list_of_books:
             if book.get_id() == id:
                 return book.return_book()
 
-
-    def overdue_books(self)->list[Book]:
+    def overdue_books(self) -> list[Book]:
+        """
+        Looks for all the overdue books based on the current date.
+        :return: A list of book objects of all the overdue books.
+        """
         today = date.today()
         list_of_overdue_books = []
-
 
         for book in self.list_of_books:
             if book.get_due_date() != None:
@@ -87,11 +113,10 @@ class Library:
                 if today > book_due_date:
                     list_of_overdue_books.append(book)
 
-        
         return list_of_overdue_books
 
 
-                
+
 
 
 
